7fdca681f7a59d749c3218fb7229f2611cbc6b29
Picalines <picalines0@gmail.com>
2023-12-04 14:36:13 +0300

    scrap .Web project, separate API from frontend

---
+++ DiagramEditor.Web.API/Program.cs
@@ -0,0 +1,37 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("appsettings.Secret.json");
+builder.Configuration.AddEnvironmentVariables();
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddControllers();
+
+builder.Services.UseSwagger();
+builder.Services.UseDistributedCache(builder.Configuration);
+builder.Services.UseJwtAuthentication(builder.Configuration);
+
+builder.Services.AddCors(
+    options => options.AddPolicy("AllowAll", builder => builder
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowAnyOrigin())
+);
+
+var app = builder.Build();
+
+/* app.UseHttpsRedirection(); */
+app.UseCors("AllowAll");
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
